0ad446f9f323a348b4b31c51508474989b52d559
Clay Miller <clay@smockle.com>
2024-07-29 12:46:54 -0400

    wip: Refactor to use 'Message' class. Add JSDoc comments. Rename 'new-message' event to avoid naming conflicts.

--- ariaNotify-polyfill.js
+++ ariaNotify-polyfill.js
@@ -1,63 +1,153 @@
+// @ts-check
+
 if (!("ariaNotify" in Element.prototype)) {
-  let uniqueId = Date.now();
+  /** @type {string} */
+  let uniqueId = `${Date.now()}`;
   try {
     uniqueId = crypto.randomUUID();
   } catch {}
+
+  /** @type {string} */
   const liveRegionCustomElementName = `live-region-${uniqueId}`;
 
   class MessageEvent extends Event {
+    /**
+     * @param {string} type
+     * @param {object} options
+     * @param {string} options.message
+     */
     constructor(type, { message, ...options }) {
       super(type, options);
       this.message = message;
     }
   }
 
+  class Message {
+    /** @type {Element} */
+    element;
+
+    /** @type {string} */
+    message;
+
+    /** @type {"important" | "none"} */
+    priority = "none";
+
+    /** @type {"all" | "pending" | "none"} */
+    interrupt = "none";
+
+    /**
+     * @param {object} message
+     * @param {Element} message.element
+     * @param {string} message.message
+     * @param {"important" | "none"} message.priority
+     * @param {"all" | "pending" | "none"} message.interrupt
+     */
+    constructor({ element, message, priority, interrupt }) {
+      this.element = element;
+      this.message = message;
+      this.priority = priority;
+      this.interrupt = interrupt;
+    }
+
+    /**
+     * Whether this message and the given message are equivalent.
+     * @param {Message} message
+     * @returns {boolean}
+     */
+    matches(message) {
+      return (
+        this.element === message.element &&
+        this.priority === message.priority &&
+        this.interrupt === message.interrupt
+      );
+    }
+
+    /**
+     * Send a 'new-message…' event with this message’s message.
+     * @returns {void}
+     */
+    announce() {
+      this.element.dispatchEvent(
+        new MessageEvent(`new-message-${uniqueId}`, {
+          message: this.message ?? "",
+        })
+      );
+    }
+
+    /**
+     * Send a 'new-message…' event with an empty message.
+     * @returns {void}
+     */
+    destroy() {
+      this.element.dispatchEvent(
+        new MessageEvent(`new-message-${uniqueId}`, {
+          message: "",
+        })
+      );
+    }
+  }
+
   class MessageQueue {
-    #element;
+    /** @type {Message[]} */
     #queue = [];
-    #interval = 500;
+
+    /** @type {Message | undefined | null} */
+    #currentMessage;
+
+    /** @type {number} */
+    #interval = 500; // TODO: Vary based on message length.
+
+    /** @type {number} */
     #intervalId = setInterval(() => this.#dequeue(), this.#interval);
 
-    constructor(element) {
-      this.#element = element;
-    }
-
+    /**
+     * Destroy the current message, then announce the next message.
+     * @returns {void}
+     */
     #dequeue() {
-      const { message } = this.#queue.shift() ?? {};
-      if (message) {
-        this.#element.dispatchEvent(
-          new MessageEvent("new-message", { message })
-        );
-      }
+      this.#currentMessage?.destroy();
+      this.#currentMessage = this.#queue.shift();
+      this.#currentMessage?.announce();
     }
 
-    enqueue({ element, message, priority, interrupt }) {
+    /**
+     * Add the given message to the queue.
+     * @param {Message} message
+     * @returns {void}
+     */
+    enqueue(message) {
+      const { priority, interrupt } = message;
+
+      if (interrupt === "all" && this.#currentMessage?.matches(message)) {
+        // Immediately flush the current message
+        this.#currentMessage?.destroy();
+        this.#currentMessage = null;
+      }
+
       if (interrupt === "all" || interrupt === "pending") {
         // Remove other messages with the same element, priority, and interrupt
         this.#queue = this.#queue.filter(
-          (message) =>
-            message.element !== element ||
-            message.priority !== priority ||
-            message.interrupt !== interrupt
+          (message) => !message.matches(message)
         );
       }
 
       if (priority === "important") {
         // Insert after the last important message, or at the beginning
+        // @ts-ignore: ts(2550)
         const lastImportantMessage = this.#queue.findLastIndex(
           (message) => message.priority === "important"
         );
-        this.#queue.splice(lastImportantMessage + 1, 0, {
-          element,
-          message,
-          priority,
-        });
+        this.#queue.splice(lastImportantMessage + 1, 0, message);
       } else {
         // Insert at the end
-        this.#queue.push({ element, message, priority });
+        this.#queue.push(message);
       }
     }
 
+    /**
+     * Remove messages associated with the given element from the queue.
+     * @param {Node} element
+     */
     flushElement(element) {
       this.#queue = this.#queue.filter(
         (message) => message.element !== element
@@ -67,13 +157,16 @@ if (!("ariaNotify" in Element.prototype)) {
 
   customElements.define(
     liveRegionCustomElementName,
-    class extends HTMLElement {
-      #queue = new MessageQueue(this);
+    class LiveRegionCustomElement extends HTMLElement {
+      /** @type {MessageQueue} */
+      #queue = new MessageQueue();
 
+      /** @type {MutationObserver} */
       #removedElementObserver = new MutationObserver((mutationList) => {
         for (const mutation of mutationList) {
           if (mutation.type === "childList") {
             for (const removedNode of mutation.removedNodes) {
+              // Remove messages associated with the removed element from the queue.
               this.#queue.flushElement(removedNode);
             }
           }
@@ -83,27 +176,46 @@ if (!("ariaNotify" in Element.prototype)) {
       connectedCallback() {
         this.role = "status";
         this.ariaLive = "polite";
-        this.style = "position: absolute; left: -9999px;";
-        this.addEventListener("new-message", this);
+        this.style.position = "absolute";
+        this.style.left = "-9999px";
+        this.addEventListener(`new-message-${uniqueId}`, this);
         this.#removedElementObserver.observe(document.body, {
           childList: true,
           subtree: true,
-          removedNodes: true,
         });
       }
 
       handleEvent(event) {
-        if (event.type === "new-message") {
+        if (event.type === `new-message-${uniqueId}`) {
           this.textContent = event.message;
         }
       }
 
-      notifyFromElement(element, message, { priority, interrupt } = {}) {
-        this.#queue.enqueue({ element, message, priority, interrupt });
+      /**
+       * @param {Element} element
+       * @param {string} message
+       * @param {object} options
+       * @param {"important" | "none"} options.priority
+       * @param {"all" | "pending" | "none"} options.interrupt
+       */
+      notifyFromElement(element, message, { priority, interrupt }) {
+        this.#queue.enqueue(
+          new Message({
+            element,
+            message,
+            // Ensure values are valid
+            priority: priority === "important" ? "important" : "none",
+            interrupt:
+              interrupt === "all" || interrupt === "pending"
+                ? interrupt
+                : "none",
+          })
+        );
       }
     }
   );
 
+  // @ts-ignore: ts(2339)
   Element.prototype.ariaNotify = function (
     message,
     { priority = "none", interrupt = "none" } = {}
@@ -118,6 +230,7 @@ if (!("ariaNotify" in Element.prototype)) {
     }
 
     // Add message to 'live-region'’s queue
+    // @ts-ignore: ts(2339)
     liveRegion.notifyFromElement(this, message, { priority, interrupt });
   };
 }
